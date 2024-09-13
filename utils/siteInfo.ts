614c34d6b4a8a46226cbc20555c477ca23df4629
El diego <franponce98@gmail.com>
2024-09-12 23:16:48 -0300

    Se arregla error de llaves en site info

--- utils/siteInfo.ts
+++ utils/siteInfo.ts
@@ -13,7 +13,24 @@ export interface SiteInformation {
 }
 
 export const getDefaultSiteInformation = (): SiteInformation => ({
-  // ... (mantén el contenido existente)
+  title: "Manos de manteca",
+  description: "Envianos tu pedido y a la brevedad te respondemos.",
+  description2: "👉 Nuestro horario de atención es de X a X de X a X hs. Hacemos envíos 🚴‍♀",
+  whatsappCart: "54929542201999",
+  sheet: "https://docs.google.com/spreadsheets/d/e/2PACX-1vReSQMLVR-O0uKqZr28Y9j29RN1YYoaFkb29qVJjofGNZSRUnhCsgoohDDDrsAV0FW4R9xdulrn0aYE/pub?output=csv",
+  color: "teal",
+  social: [
+    {
+      name: "instagram",
+      url: "https://www.hola.com"
+    },
+    {
+      name: "whatsapp",
+      url: "https://wa.me/54929542201999"
+    }
+  ],
+  banner: "",
+  avatar: ""
 });
 
 export const getSiteInformation = async (): Promise<SiteInformation> => {
