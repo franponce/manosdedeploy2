b13ff0b1e01bfc2af3fab06810e13b8449ddbc40
El diego <franponce98@gmail.com>
2024-09-14 18:21:45 -0300

    Probamos el enfoque de SSR con revalidación aprovechando como usamos vercel KV

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,41 @@ import { GetStaticProps } from 'next';
 import { Product } from '../product/types';
 import api from '../product/api';
 import StoreScreen from '../product/screens/Store';
+import { getSiteInformation, SiteInformation } from '../utils/siteInfo';
 
 interface Props {
   products: Product[];
+  siteInfo: SiteInformation;
 }
 
-const IndexRoute: React.FC<Props> = ({ products }) => {
-  return <StoreScreen products={products} />;
+const IndexRoute: React.FC<Props> = ({ products, siteInfo }) => {
+  return <StoreScreen products={products} siteInfo={siteInfo} />;
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const products = await api.list();
-  return {
-    props: {
-      products,
-    },
-    revalidate: 10,
-  };
+  try {
+    const [products, siteInfo] = await Promise.all([
+      api.list(),
+      getSiteInformation()
+    ]);
+
+    return {
+      props: {
+        products,
+        siteInfo,
+      },
+      revalidate: 60, // Revalidar cada minuto
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      props: {
+        products: [],
+        siteInfo: {},
+      },
+      revalidate: 60,
+    };
+  }
 };
 
 export default IndexRoute;
--- product/screens/Store.tsx
+++ product/screens/Store.tsx
@@ -6,18 +6,21 @@ import {
   Stack,
   Text,
   Box,
+  Heading,
 } from "@chakra-ui/react";
 import { CartItem, Product } from "../types";
 import ProductCard from "../components/ProductCard";
 import CartDrawer from "../components/CartDrawer";
 import { editCart } from "../selectors";
 import { parseCurrency } from "../../utils/currency";
+import { SiteInformation } from "../../utils/siteInfo";
 
 interface Props {
   products: Product[];
+  siteInfo: SiteInformation;
 }
 
-const StoreScreen: React.FC<Props> = ({ products }) => {
+const StoreScreen: React.FC<Props> = ({ products, siteInfo }) => {
   const [cart, setCart] = React.useState<CartItem[]>([]);
   const [isCartOpen, toggleCart] = React.useState<boolean>(false);
 
@@ -40,6 +43,11 @@ const StoreScreen: React.FC<Props> = ({ products }) => {
   return (
     <>
       <Stack spacing={6}>
+        <Box>
+          <Heading as="h1" size="xl" mb={2}>{siteInfo.title}</Heading>
+          <Text fontSize="lg" mb={4}>{siteInfo.description}</Text>
+          <Text fontSize="md" mb={6}>{siteInfo.description2}</Text>
+        </Box>
         {validProducts.length ? (
           <Grid
             gridGap={8}
