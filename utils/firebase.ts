4ff71fefbf94bb87ad07e313652feff664083233
El diego <franponce98@gmail.com>
2024-10-07 23:11:20 -0300

    Cambiamos número de whatsapp

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -31,7 +31,7 @@ export const DEFAULT_SITE_INFORMATION: SiteInformation = {
     title: "Manos de manteca",
     description: "Envianos tu pedido y a la brevedad te respondemos.",
     description2: "👉 Nuestro horario de atención es de X a X de X a X hs. Hacemos envíos 🚴‍♀",
-    whatsappCart: "54929542201999",
+    whatsappCart: "5492954271140",
     sheet: "https://docs.google.com/spreadsheets/d/e/2PACX-1vReSQMLVR-O0uKqZr28Y9j29RN1YYoaFkb29qVJjofGNZSRUnhCsgoohDDDrsAV0FW4R9xdulrn0aYE/pub?output=csv",
     color: "teal",
     social: [
