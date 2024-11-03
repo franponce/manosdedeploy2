d8e714b57259542fbe4695c51256ff2d7411205a
El diego <franponce98@gmail.com>
2024-11-03 03:32:32 -0300

    Cambiamos la label del producto programado para que sea más informativo

--- product/components/ProductManagement.tsx
+++ product/components/ProductManagement.tsx
@@ -381,7 +381,15 @@ const ProductManagement: React.FC<ProductManagementProps> = ({
                   left="2" 
                   zIndex="1"
                 >
-                  Producto programado
+                  Producto programado para el{' '}
+                  {new Date(product.scheduledPublishDate!).toLocaleString('es-AR', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    hour12: false
+                  })}
                 </Badge>
               )}
               <AspectRatio ratio={1}>
