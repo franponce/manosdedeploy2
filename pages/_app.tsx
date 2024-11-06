92f5b145a13cbf7e9ddd0e2d390eeb213ee0df11
franponce <franponce98@gmail.com>
2024-11-06 20:45:21 -0300

    feat: ocultar header y site info en mobile para página de login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -112,6 +112,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     router.push('/admin');
   };
 
+  const isLoginPage = router.pathname === '/login';
+  const shouldShowSiteInfo = !isLoginPage || (isLoginPage && window.innerWidth >= 768);
+
   const getWelcomeMessage = () => {
     if (router.pathname === '/store-config') {
       return (
@@ -123,6 +126,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           Volver a la gestión de productos
         </Button>
       );
+    }
+    if (router.pathname === '/login') {
+      return 'Simple E-commerce';
     }
     if (router.pathname === '/admin') {
       if (isAdmin) {
@@ -162,7 +168,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <script dangerouslySetInnerHTML={{ __html: customScripts }} />
         )}
       </Head>
-      {announcementBar && announcementBar.isEnabled && (
+      {!isLoginPage && announcementBar && announcementBar.isEnabled && (
         <Box bg="blue.500" color="white" py={2}>
           <Container maxW="container.xl">
             <Flex justify="space-between">
@@ -177,118 +183,124 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           </Container>
         </Box>
       )}
-      <Box
-        position="fixed"
-        top={0}
-        left={0}
-        right={0}
-        zIndex={1000}
-        bg="white"
-        boxShadow="md"
-      >
-        <Container maxWidth="container.xl" padding={4}>
-          <Flex alignItems="center" justifyContent="space-between">
-            <NextLink href="/" legacyBehavior>
-              <Heading as="a" size="md" cursor="pointer">
-                {getWelcomeMessage()}
-              </Heading>
-            </NextLink>
-            {isMounted && (
-              <HamburgerMenu
-                isLoggedIn={isLoggedIn}
-                isAdmin={isAdmin}
-                onLogout={handleLogout}
-              />
-            )}
-          </Flex>
-        </Container>
-      </Box>
-      <Box pt="70px"> {/* Ajusta este valor según la altura de tu header fijo */}
+      {!isLoginPage && (
+        <Box
+          position="fixed"
+          top={0}
+          left={0}
+          right={0}
+          zIndex={1000}
+          bg="white"
+          boxShadow="md"
+        >
+          <Container maxWidth="container.xl" padding={4}>
+            <Flex alignItems="center" justifyContent="space-between">
+              <NextLink href="/" legacyBehavior>
+                <Heading as="a" size="md" cursor="pointer">
+                  {getWelcomeMessage()}
+                </Heading>
+              </NextLink>
+              {isMounted && (
+                <HamburgerMenu
+                  isLoggedIn={isLoggedIn}
+                  isAdmin={isAdmin}
+                  onLogout={handleLogout}
+                />
+              )}
+            </Flex>
+          </Container>
+        </Box>
+      )}
+      <Box pt={isLoginPage ? 0 : "70px"}>
         <Container
           backgroundColor="white"
           borderRadius="sm"
           maxWidth="container.xl"
           padding={4}
         >
-          <Box mb={6} position="relative">
-            <Box
-              borderRadius="lg"
-              height={{ md: "300px" }}
-              overflow="hidden"
-              width="100%"
-              position="relative"
-            >
-              <Image
-                src={bannerError ? "/default-banner.jpg" : `${siteInfo?.bannerUrl}?${new Date().getTime()}`}
-                alt="Header image"
-                objectFit="cover"
-                width="100%"
-                height="100%"
-                onError={() => setBannerError(true)}
-                fallback={<Box bg="gray.200" w="100%" h="100%" />}
-              />
-            </Box>
-          </Box>
-          <Flex
-            direction="column"
-            align="center"
-            justify="center"
-            mb={6}
-          >
-            <Box
-              backgroundColor="white"
-              borderRadius="full"
-              boxShadow="md"
-              boxSize="120px"
-              overflow="hidden"
-              position="relative"
-              mb={4}
-            >
-              <Image
-                src={`${siteInfo?.logoUrl}?${new Date().getTime()}`}
-                alt="Avatar"
-                objectFit="cover"
-                width="100%"
-                height="100%"
-                fallback={<Box bg="gray.200" w="100%" h="100%" borderRadius="full" />}
-              />
-            </Box>
-            <Stack
-              align="center"
-              bg="white"
-              borderRadius="md"
-              boxShadow="sm"
-              p={4}
-              spacing={3}
-              textAlign="center"
-              width="100%"
-              maxWidth="600px"
-            >
-              <Heading size="lg">{siteInfo?.title}</Heading>
-              <Text color="gray.600" fontSize="md" dangerouslySetInnerHTML={{ __html: siteInfo?.description || '' }} />
-              <Text color="gray.600" fontSize="md" dangerouslySetInnerHTML={{ __html: siteInfo?.description2 || '' }} />
-              <Stack direction="row" mt={2} spacing={2} justify="center">
-                {siteInfo?.social?.map((social) => (
-                  <Link key={social.name} href={social.url} isExternal>
-                    <Flex
-                      alignItems="center"
-                      backgroundColor="#df7777"
-                      borderRadius="full"
-                      color="white"
-                      height={8}
-                      justifyContent="center"
-                      width={8}
-                    >
-                      <Image 
-                        alt={`${social.name} icon`}
-                        src={`https://icongr.am/fontawesome/${social.name}.svg?size=20&color=ffffff`}
-                      />
-                    </Flex>
-                  </Link>
-                ))}
-              </Stack>
-            </Stack>
-          </Flex>
+          {shouldShowSiteInfo && (
+            <>
+              <Box mb={6} position="relative">
+                <Box
+                  borderRadius="lg"
+                  height={{ md: "300px" }}
+                  overflow="hidden"
+                  width="100%"
+                  position="relative"
+                >
+                  <Image
+                    src={bannerError ? "/default-banner.jpg" : `${siteInfo?.bannerUrl}?${new Date().getTime()}`}
+                    alt="Header image"
+                    objectFit="cover"
+                    width="100%"
+                    height="100%"
+                    onError={() => setBannerError(true)}
+                    fallback={<Box bg="gray.200" w="100%" h="100%" />}
+                  />
+                </Box>
+              </Box>
+              <Flex
+                direction="column"
+                align="center"
+                justify="center"
+                mb={6}
+              >
+                <Box
+                  backgroundColor="white"
+                  borderRadius="full"
+                  boxShadow="md"
+                  boxSize="120px"
+                  overflow="hidden"
+                  position="relative"
+                  mb={4}
+                >
+                  <Image
+                    src={`${siteInfo?.logoUrl}?${new Date().getTime()}`}
+                    alt="Avatar"
+                    objectFit="cover"
+                    width="100%"
+                    height="100%"
+                    fallback={<Box bg="gray.200" w="100%" h="100%" borderRadius="full" />}
+                  />
+                </Box>
+                <Stack
+                  align="center"
+                  bg="white"
+                  borderRadius="md"
+                  boxShadow="sm"
+                  p={4}
+                  spacing={3}
+                  textAlign="center"
+                  width="100%"
+                  maxWidth="600px"
+                >
+                  <Heading size="lg">{siteInfo?.title}</Heading>
+                  <Text color="gray.600" fontSize="md" dangerouslySetInnerHTML={{ __html: siteInfo?.description || '' }} />
+                  <Text color="gray.600" fontSize="md" dangerouslySetInnerHTML={{ __html: siteInfo?.description2 || '' }} />
+                  <Stack direction="row" mt={2} spacing={2} justify="center">
+                    {siteInfo?.social?.map((social) => (
+                      <Link key={social.name} href={social.url} isExternal>
+                        <Flex
+                          alignItems="center"
+                          backgroundColor="#df7777"
+                          borderRadius="full"
+                          color="white"
+                          height={8}
+                          justifyContent="center"
+                          width={8}
+                        >
+                          <Image 
+                            alt={`${social.name} icon`}
+                            src={`https://icongr.am/fontawesome/${social.name}.svg?size=20&color=ffffff`}
+                          />
+                        </Flex>
+                      </Link>
+                    ))}
+                  </Stack>
+                </Stack>
+              </Flex>
+            </>
+          )}
           <Component {...pageProps} />
           <Divider marginY={4} />
           <Text textAlign="center">
